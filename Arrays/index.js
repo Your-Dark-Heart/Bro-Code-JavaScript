77bae4e4c7e57acbc1cad9df1e09fca1dfd05ca7
Your-Dark-Heart <melikovasliddin555@gmail.com>
2024-07-05 16:24:50 +0500

    Arrays

---
+++ Arrays/index.js
@@ -0,0 +1,37 @@
+let fruits = ["apple", "orange", "banana"];
+
+
+fruits[3] = "coconut";
+
+
+// push
+fruits.push("chocolate");
+
+
+
+// pop
+const chocolate = fruits.pop();
+
+
+// length
+const numOfFruits = fruits.length;
+
+
+
+// indexOf
+const indexOfApple = fruits.indexOf("apple");
+const indexOfNothing = fruits.indexOf("mango")
+
+
+// looping
+for (let i = fruits.length - 1; i >= 0; i--) {
+  console.log(fruits[i]);
+}
+
+
+// sort
+fruits.sort();
+
+
+// reverse
+fruits.reverse();

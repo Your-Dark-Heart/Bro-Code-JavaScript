c839ee6c3780918dad0aaaa1900026fbbdff8124
Your-Dark-Heart <melikovasliddin555@gmail.com>
2024-07-05 17:23:41 +0500

    Spread Operator

---
+++ Spread Operator/index.js
@@ -0,0 +1,17 @@
+let numbers = [1, 2, 3, 4, 5];
+
+
+let maximum = Math.max(...numbers);
+let minimum = Math.min(...numbers);
+
+let userName = "Dark Heart";
+let letters = [...userName].join("-");
+
+
+let fruits = ["apple", "orange", "banana"];
+let vegetables = ["carrots", "celery", "potatoes"];
+
+let foods = [...fruits, ...vegetables, "eggs", "milk"];
+
+
+console.log(foods);

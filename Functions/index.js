5c7014d15aff87821c512dcab5c6faef34db8733
Your-Dark-Heart <melikovasliddin555@gmail.com>
2024-07-05 15:18:34 +0500

    Functions

---
+++ Functions/index.js
@@ -0,0 +1,33 @@
+
+function isValidEmail(email) {
+  return email.includes("@") 
+}
+
+console.log(isValidEmail("melikovasliddin555@gmail.com"));
+
+/* 
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+console.log(isEven(100)); */
+
+/* 
+function add(x, y) {
+  return x + y
+}
+
+console.log(add(2, 3) / 100);
+ */
+
+/* 
+function happyBirthday(username, age) {
+  console.log("Happy birthday to you!");
+  console.log("Happy birthday to you!");
+  console.log(`Happy birthday dear ${username}!`);
+  console.log("Happy birthday to you!");
+  console.log(`You are ${age} years old!`);
+}
+
+happyBirthday("Dark Heart", 18);
+*/

0835d4da15e5a9bdfafaa90d37cd1c60a077e02b
Your-Dark-Heart <melikovasliddin555@gmail.com>
2024-07-07 11:48:53 +0500

    Objects

---
+++ Objects/index.js
@@ -0,0 +1,26 @@
+const person1 = {
+  firstName: "Spongebob",
+  lastName: "Squarepants",
+  age: 30,
+  isEmployed: true,
+  sayHello() {
+    console.log("Hi! I am Spongebob!")
+  },
+  eats() {
+    console.log("I am eating a Krabby Patty!");
+  }
+}
+
+const person2 = {
+  firstName: "Patrick",
+  lastName: "Star",
+  age: 42,
+  isEmployed: false,
+  sayHello() {
+    console.log("Hey, I am Patrick...");
+  },
+  eats() {
+    console.log("I am eating a roast beef, chicken, and pizza!");
+  }
+}
+

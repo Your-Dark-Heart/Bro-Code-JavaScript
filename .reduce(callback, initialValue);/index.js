e98b95a8814a3c4ee0a6ec69876328bcd90e538e
Your-Dark-Heart <melikovasliddin555@gmail.com>
2024-07-07 10:33:21 +0500

    reduce(accumulator, element, index, array)

--- .reduce(callback, initialValue);/index.js
+++ .reduce(callback, initialValue);/index.js
@@ -1,15 +1,72 @@
 // .reduce() = reduce the elements of the array to a single value
 
 
+
+const numbers = [13, 2, 5];
+
+const sum = numbers.reduce((total, number, index, array) => {
+  return total + number;
+}, 0);
+
+// But a default value in case the array may be empty
+
+console.log(sum);
+
+
+
+
+/* 
+const people = [
+  {name: "Kyle", age: 26},
+  {name: "John", age: 31},
+  {name: "Sally", age: 42},
+  {name: "Jill", age: 42}
+]
+
+
+const result = people.reduce((groupedPeople, person) => {
+  const age = person.age;
+  if (groupedPeople[age] == null) {
+    groupedPeople[age] = []
+  } groupedPeople[age].push(person);
+  
+  
+  return groupedPeople;
+}, {});
+
+console.log(result); */
+
+
+/*
+const items = [
+  {name: "Rice", price: 5},
+  {name: "Book", price: 20},
+  {name: "Chicken", price: 10},
+  {name: "Monitor", price: 100}
+]
+
+
+// let totalPrice = 0;
+// items.forEach(item => {
+//   totalPrice += item.price
+// })
+
+const totalPrice = items.reduce((total, item)=>{
+  return total + item.price
+}, 0);
+
+
+console.log(totalPrice);
+*/
+
+
+/* 
 const grades = [75, 500, 90, 80, 65, 95];
 
 const maxium = grades.reduce(getMax, 100);
 const minimum = grades.reduce(getMin, 2);
 
 console.log(minimum);
-
-
-
 
 
 function getMin(min, element) {
@@ -20,9 +77,10 @@ function getMin(min, element) {
 function getMax(accumulator, element) {
   return Math.max(accumulator, element);
 }
+ */
 
 
-
+/* 
 const prices = [5, 30, 10, 25, 15, 20];
 const total = prices.reduce(sum, 1000);
 
@@ -31,4 +89,4 @@ console.log(`$${total.toFixed(2)}`);
 
 function sum(total, element) {
   return total + element;
-}
+} */

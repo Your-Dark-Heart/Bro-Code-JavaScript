04a52c8a36cdd6cf65853fa90753635ff21c670c
Your-Dark-Heart <melikovasliddin555@gmail.com>
2024-07-19 22:07:55 +0500

    Callback Hell

--- Asynchronous/Callback Hell/index.js
+++ Asynchronous/Callback Hell/index.js
@@ -1,3 +1,65 @@
+
+
+const stocks = {
+  fruits: ["strawberry", "grapes", "banana", "apple"],
+  liquid: ["milk", "water"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "peanuts"]
+};
+
+
+
+function order(fruitIndex, callProduction) {
+
+  setTimeout(() => {
+    console.log(`${stocks.fruits[fruitIndex].toUpperCase()} was selected!`);
+    callProduction();
+  }, 2000);
+}
+
+
+function production() {
+  setTimeout(() => {
+    console.log(`Production has started!`);
+
+    setTimeout(() => {
+      console.log(`The fruit has been chopped!`);
+
+      setTimeout(() => {
+        console.log(`${stocks.liquid[0].toUpperCase()} and ${stocks.liquid[1].toUpperCase()} were added!`);
+
+        setTimeout(() => {
+          console.log(`The machine started!`);
+
+          setTimeout(() => {
+            console.log(`The ice cream was selected on a ${stocks.holder[0]}.`);
+
+            setTimeout(() => {
+              console.log(`${stocks.toppings[0].toUpperCase()} and ${stocks.toppings[1].toUpperCase()} were added as toppings!`);
+
+              setTimeout(() => {
+                console.log(`Serve the ice cream!`);
+              }, 2000);
+            }, 3000);
+          }, 2000);
+        }, 1000);
+      }, 1000);
+    }, 2000);
+  }, 0);
+}
+
+order(0, production);
+
+
+
+
+
+
+
+
+
+
+/* 
 function task1(callback) {
   setTimeout(() => {
     console.log("Task 1 complete.");
@@ -33,4 +95,5 @@ task1(() => {
       });
     });
   });
-});
+}); 
+*/

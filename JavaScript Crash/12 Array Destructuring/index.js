bebb3d01b74f4080398ba46aff0332de0d1b68b9
Your-Dark-Heart <melikovasliddin555@gmail.com>
2024-07-23 13:53:16 +0500

    Array Destructuring

---
+++ JavaScript Crash/12 Array Destructuring/index.js
@@ -0,0 +1,34 @@
+// Unpacking Values
+/*
+const fruits = ["orange", "banana", "lemon"];
+const friends = ["john", "peter", "bob", "anna", "kelly"];
+
+//const [john, peter, bob, anna, kelly] = friends;
+
+
+const [john, , , anna, kelly] = friends;
+
+
+console.log(john);
+
+*/
+
+
+
+
+
+// Swapping Values
+
+let firstName = "Asliddin";
+let userName = "Dark Heart";
+
+
+// let temporary = firstName;
+// firstName = userName;
+// userName = temporary;
+
+[userName, firstName] = [firstName, userName];
+
+console.log(userName);
+console.log(firstName);
+
